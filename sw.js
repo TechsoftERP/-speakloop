595540869ded74d5ab5ef75a71e39bd9f90a046f
TechsoftERP <ridvan@techsoftyazilim.com.tr>
2026-09-21 21:55:46 +0300

    Bump SpeakLoop cache to V10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='speakloop-pwa-9.2.0';
+const VERSION='speakloop-pwa-10.0.0';
 const APP='./index.html';
 const STATIC=['./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 
